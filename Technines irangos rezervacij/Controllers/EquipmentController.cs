a4092c32809ba070c4d89eb5b879204399f183fa
Ieva <ieva.jaksaityte@gmail.com>
2018-04-03 16:56:41 +0300

    Added more views.

---
+++ Technines irangos rezervacij/Controllers/EquipmentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Technines_irangos_rezervacij.Controllers
+{
+    public class EquipmentController : Controller
+    {
+        // GET: Equipment
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Equipment()
+        {
+            return View();
+        }
+
+        public ActionResult Bundles()
+        {
+            return View();
+        }
+
+        public ActionResult ForRent()
+        {
+            return View();
+        }
+    }
+}
