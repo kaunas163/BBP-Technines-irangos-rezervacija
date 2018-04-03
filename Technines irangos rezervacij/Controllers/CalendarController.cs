61d62f86b7e23b56a4555c3e0d8dd7dac66b4a11
Ieva <ieva.jaksaityte@gmail.com>
2018-04-03 13:41:43 +0300

    Created some essential views

---
+++ Technines irangos rezervacij/Controllers/CalendarController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Technines_irangos_rezervacij.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // GET: Calendar
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Day()
+        {
+            return View();
+        }
+
+        public ActionResult Week()
+        {
+            return View();
+        }
+
+        public ActionResult Month()
+        {
+            return View();
+        }
+    }
+}
---
+++ Technines irangos rezervacij/Controllers/RezervationsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Technines_irangos_rezervacij.Controllers
+{
+    public class RezervationsController : Controller
+    {
+        // GET: Rezervations
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        public ActionResult Pending()
+        {
+            return View();
+        }
+
+        public ActionResult Active()
+        {
+            return View();
+        }
+
+        public ActionResult Archived()
+        {
+            return View();
+        }
+    }
+}
